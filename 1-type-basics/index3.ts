456b72d6ba41eba9758280208e446126c77b2a0b
Isa Reinert <reinert.isa@gmail.com>
2025-05-31 13:23:41 -0700

    feat(ts): array types

---
+++ 1-type-basics/index3.ts
@@ -0,0 +1,24 @@
+// Arrays
+
+let names: string[] = ['mario', 'luigi', 'Inigo'];
+names.push('cat');
+
+let ages: number[] = [10, 12, 100];
+ages.push(99);
+
+let flags: boolean[] = [true, false];
+flags.push(true);
+
+// type interference with arrays
+let fruits = ['apples', 'pears', 'bananas', 'mango']
+fruits.push('peaches')
+
+const fruit = fruits[3];
+
+let things = [1, true, 'hello'];
+const t = things[0];
+
+
+
+
+
---
+++ dist/1-type-basics/index3.js
@@ -0,0 +1,14 @@
+"use strict";
+// Arrays
+let names = ['mario', 'luigi', 'Inigo'];
+names.push('cat');
+let ages = [10, 12, 100];
+ages.push(99);
+let flags = [true, false];
+flags.push(true);
+// type interference with arrays
+let fruits = ['apples', 'pears', 'bananas', 'mango'];
+fruits.push('peaches');
+const fruit = fruits[3];
+let things = [1, true, 'hello'];
+const t = things[0];
