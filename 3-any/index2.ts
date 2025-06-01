0c908b8639f71b39f8ac006677ac20a42471cb26
Isa Reinert <reinert.isa@gmail.com>
2025-06-01 12:49:07 -0700

    feat(ts): any types

---
+++ 3-any/index2.ts
@@ -0,0 +1,15 @@
+let age3: any = 25;
+
+age3 = 'hello';
+age3 = true;
+age3 = null;
+age3 = {}
+
+
+let arr4: any[] = [1, 'hello', true, null, {}]
+arr4.push(100);
+arr4.push('hello world');
+
+let ninja4: {name: any, age: any} = {name: 'Inigo', age: 20}
+ninja4.name = 19;
+ninja4.age = '20';
